c35d1298a721f5805b3b82c339990bdeb875a843
broeker <tim@electriccitizen.com>
2019-06-12 20:18:45 -0500

    JS: header update.

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -87,7 +87,7 @@ export const IndexPageTemplate = ({
                 <div className="columns">
                   <div className="column is-12">
                     <h3 className="has-text-weight-semibold is-size-2">
-                    xxx
+                      { main.heading }
                     </h3>
                     <div
                       dangerouslySetInnerHTML={{ __html: main.description }}
